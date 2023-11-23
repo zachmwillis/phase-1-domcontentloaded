c60cda658fb403f81155b215509c23757c69f18b
Zach Willis <zachmw27@gmail.com>
2023-11-23 16:53:56 -0500

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const textParagraph = document.getElementById("text");
+    textParagraph.textContent = "This is really cool!";
+});
